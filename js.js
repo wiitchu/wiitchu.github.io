337cdd228d4f9cf6d5cec9ee749a77c52dbd3fab
Christina <christina.gushcheva@gmail.com>
2019-05-02 22:30:11 +0300

    Commit 2

---
+++ js.js
@@ -0,0 +1,128 @@
+
+
+/**
+ * demo3.js
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ * 
+ * Copyright 2017, Codrops
+ * http://www.codrops.com
+ */
+
+
+{
+    class MorphingBG {
+        constructor(el) {
+            this.DOM = {};
+            this.DOM.el = el;
+            this.DOM.paths = Array.from(this.DOM.el.querySelectorAll('path'));
+            this.animate();
+        }
+        animate() {
+            this.DOM.paths.forEach((path) => {
+                setTimeout(() => {
+                    anime({
+                        targets: path,
+                        duration: anime.random(3000,5000),
+                        easing: [0.5,0,0.5,1],
+                        d: path.getAttribute('pathdata:id'),
+                        loop: true,
+                        direction: 'alternate'
+                    });
+                }, anime.random(0,1000));
+            });
+        }
+    };
+
+    new MorphingBG(document.querySelector('svg.scene'));
+};
+
+window.addEventListener("beforeunload", function () {
+  document.body.classList.add("animate-out");
+});
+
+
+// eye animation 
+
+var eyeanimation = lottie.loadAnimation({
+  container: document.getElementById("lottie"),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'js/data.json' // the path to the animation json
+});
+
+
+
+// parallax //
+
+window.addEventListener('scroll', function (e) {
+    var scrolled = window.pageYOffset;
+    const background = document.querySelector('.intro');
+    background.style.top = - (scrolled * 0.3) + "px";
+});
+
+
+
+// fade out //
+
+
+$(window).scroll(function(){
+    $('.onscroll').css('opacity', 1 - $(window).scrollTop() / 500);
+  });
+
+
+// dot //
+
+
+jQuery(document).ready(function($){
+    var contentSections = $('.cd-section'),
+        navigationItems = $('#cd-vertical-nav a');
+
+    updateNavigation();
+    $(window).on('scroll', function(){
+        updateNavigation();
+    });
+
+    //smooth scroll to the section
+    navigationItems.on('click', function(event){
+        event.preventDefault();
+        smoothScroll($(this.hash));
+    });
+    //smooth scroll to second section
+    $('.cd-scroll-down').on('click', function(event){
+        event.preventDefault();
+        smoothScroll($(this.hash));
+    });
+
+    //open-close navigation on touch devices
+    $('.touch .cd-nav-trigger').on('click', function(){
+        $('.touch #cd-vertical-nav').toggleClass('open');
+  
+    });
+    //close navigation on touch devices when selectin an elemnt from the list
+    $('.touch #cd-vertical-nav a').on('click', function(){
+        $('.touch #cd-vertical-nav').removeClass('open');
+    });
+
+    function updateNavigation() {
+        contentSections.each(function(){
+            $this = $(this);
+            var activeSection = $('#cd-vertical-nav a[href="#'+$this.attr('id')+'"]').data('number') - 1;
+            if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
+                navigationItems.eq(activeSection).addClass('is-selected');
+            }else {
+                navigationItems.eq(activeSection).removeClass('is-selected');
+            }
+        });
+    }
+
+    function smoothScroll(target) {
+        $('body,html').animate(
+            {'scrollTop':target.offset().top},
+            600
+        );
+    }
+});
